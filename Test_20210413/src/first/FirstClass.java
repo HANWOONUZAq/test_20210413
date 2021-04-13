8394750d17cb997d89e89fa734b2011dd67e1062
user <user@DESKTOP-VV8Q17H>
2021-04-13 09:15:06 +0900

    First 내용수정

--- Test_20210413/src/first/FirstClass.java
+++ Test_20210413/src/first/FirstClass.java
@@ -5,7 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("돈이 필요해");
-		System.out.println("많이많이");
+		System.out.println("머리가 멍하다.");
 	}
 
 }
