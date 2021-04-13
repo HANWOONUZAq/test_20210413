46f3278c503b3d2f5264e9a1a930458e50a4e8a7
user <user@DESKTOP-VV8Q17H>
2021-04-13 09:10:21 +0900

    Second 클래스 추가 및 First 내용추가

--- Test_20210413/src/first/SecondClass.java
+++ Test_20210413/src/first/SecondClass.java
@@ -1,11 +1,11 @@
 package first;
 
-public class FirstClass {
+public class SecondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("돈이 필요해");
-		System.out.println("많이많이");
+		System.out.println("로또당첨되고싶다");
+
 	}
 
 }
